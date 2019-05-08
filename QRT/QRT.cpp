d76cdb42d55b882007ce5eb0c66edaad41d6166f
qphalcy0n <shawn.simonson@gmail.com>
2019-05-08 14:31:56 -0500

    First Push

---
+++ QRT/QRT.cpp
@@ -0,0 +1,19 @@
+
+#include <windows.h>
+#include <GL/GL.h>
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+int WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance,
+		    LPSTR lpCmdLine, __in int nShowCmd)
+{
+	return 0;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+}
